30b27362ba85d1e3cdc70f6e2a6670de79ac85ca
kianoosh <kianoosh@ezpin.com>
2024-08-01 09:15:45 +0330

    add activate

---
+++ projects/activate/src/lib/activate.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-activate',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      activate works!
+    </p>
+  `,
+  styles: ``
+})
+export class ActivateComponent {
+
+}
---
+++ projects/activate/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of activate
+ */
+
+export * from './lib/activate.service';
+export * from './lib/activate.component';
