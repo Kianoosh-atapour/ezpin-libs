75ac2c3e9397b294f49acbddc046ffccb16bf099
kianoosh <kianoosh@ezpin.com>
2024-08-07 11:49:20 +0330

    bug fix

--- projects/proxies/src/lib/proxies.module.ts
+++ projects/proxies/src/lib/proxies.module.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { ProxiesService, RetailProxyService, ShopProxyService } from "./index";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [],
   providers: [ProxiesService, ShopProxyService, RetailProxyService, HttpClient],
 })
 export class ProxiesModule {}
